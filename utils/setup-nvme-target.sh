8a99e2182f817983168c70b17d19f5b8ed766e59
Isaac Khor <s.github@isaackhor.com>
2024-05-01 21:27:27 +0000

    Fix nvmf nqns again

--- utils/setup-nvme-target.sh
+++ utils/setup-nvme-target.sh
@@ -8,6 +8,8 @@ source $zstore_dir/.env
 echo "Running gateway on $gw_ip, remote on $host_ip"
 # echo "Running with image $pool_name/$imgname"
 
+cd $zstore_dir/subprojects/spdk
+
 if pidof nvmf_tgt; then
 	scripts/rpc.py spdk_kill_instance SIGTERM >/dev/null || true
 	scripts/rpc.py spdk_kill_instance SIGKILL >/dev/null || true
@@ -16,7 +18,7 @@ if pidof nvmf_tgt; then
 	sleep 3
 fi
 
-cd $zstore_dir/subprojects/spdk
+HUGEMEM=4096 ./scripts/setup.sh
 
 modprobe nvme
 modprobe nvmet
@@ -25,13 +27,15 @@ modprobe nvmet_tcp
 ./build/bin/nvmf_tgt -m '[0,1,2,3]' &
 sleep 3
 
+ctrl_nqn="nqn.2024-04.io.zstore:cnode1"
+
 scripts/rpc.py bdev_nvme_attach_controller -b nvme0 -t PCIe -a 0000:01:00.0
 scripts/rpc.py bdev_nvme_attach_controller -b nvme1 -t PCIe -a 0000:06:00.0
 
 scripts/rpc.py nvmf_create_transport -t TCP -u 16384 -m 8 -c 8192
-scripts/rpc.py nvmf_create_subsystem nqn.2024-04.io.zstore:cnode1 -a -s SPDK00000000000001 -d SPDK_Controller1
+scripts/rpc.py nvmf_create_subsystem $ctrl_nqn -a -s SPDK00000000000001 -d SPDK_Controller1
 sleep 1
 
-scripts/rpc.py nvmf_subsystem_add_ns nqn.2024-04.io.spdk:cnode1 nvme0n2
-scripts/rpc.py nvmf_subsystem_add_ns nqn.2024-04.io.spdk:cnode1 nvme1n2
-scripts/rpc.py nvmf_subsystem_add_listener nqn.2024-04.io.zstore:cnode1 -t tcp -a $host_ip -s 23789
+scripts/rpc.py nvmf_subsystem_add_ns $ctrl_nqn nvme0n2
+scripts/rpc.py nvmf_subsystem_add_ns $ctrl_nqn nvme1n2
+scripts/rpc.py nvmf_subsystem_add_listener $ctrl_nqn -t tcp -a $host_ip -s 23789
