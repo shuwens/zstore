d96ea14d71846f20b4bd4cf752a3e01eba8bbb4c
Isaac Khor <s.github@isaackhor.com>
2024-05-02 20:35:05 +0000

    Fix nvmf script

--- utils/setup-nvme-target.sh
+++ utils/setup-nvme-target.sh
@@ -5,9 +5,6 @@ set -xeuo pipefail
 zstore_dir=$(git rev-parse --show-toplevel)
 source $zstore_dir/.env
 
-echo "Running gateway on $gw_ip, remote on $host_ip"
-# echo "Running with image $pool_name/$imgname"
-
 cd $zstore_dir/subprojects/spdk
 
 if pidof nvmf_tgt; then
@@ -41,6 +38,6 @@ sleep 1
 
 scripts/rpc.py nvmf_subsystem_add_ns $ctrl_nqn nvme0n2
 scripts/rpc.py nvmf_subsystem_add_ns $ctrl_nqn nvme1n2
-scripts/rpc.py nvmf_subsystem_add_listener $ctrl_nqn -t tcp -a $host_ip -s 23789
+scripts/rpc.py nvmf_subsystem_add_listener $ctrl_nqn -t tcp -a 10.0.0.2 -s 23789
 
 wait
