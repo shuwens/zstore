4d5c4296f57195695fef690223c2ff7dd7fec099
Shuwen Sun <sun.shuw@northeastern.edu>
2024-08-20 20:19:07 +0000

    configure

--- subprojects/packagefiles/spdk/configure-spdk.sh
+++ subprojects/packagefiles/spdk/configure-spdk.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# ./configure --disable-tests --disable-unit-tests --disable-examples --disable-apps --with-shared --with-rdma --prefix=$INSTALL_PATH --with-dpdk=$INSTALL_PATH &&
+# ./configure  --prefix=$INSTALL_PATH --with-dpdk=$INSTALL_PATH &&
 
 debug() {
   echo '===Building SPDK in debug mode...'
-  ./configure --enable-debug --with-shared # --with-rdma
+  ./configure --enable-debug --disable-tests --disable-unit-tests --disable-examples --disable-apps --with-shared --with-rdma
 }
 
 release() {
   echo '===Building SPDK in release mode...'
-  ./configure --with-shared # --with-rdma
+  ./configure --disable-tests --disable-unit-tests --disable-examples --disable-apps --with-shared --with-rdma
 }
 
 if [ $# -lt 1 ]; then
