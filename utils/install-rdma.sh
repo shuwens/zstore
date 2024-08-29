3896e486838109777dd4b0faa27850696713792a
Shuwen Sun <sun.shuw@northeastern.edu>
2024-08-29 01:10:38 +0000

    script

--- utils/install-rdma.sh
+++ utils/install-rdma.sh
@@ -12,6 +12,13 @@ sudo apt-get -y --force-yes install libibverbs1 ibverbs-utils \
 	infiniband-diags mstflint opensm perftest srptools \
 	libosmvendor5 libosmcomp5 libopensm9
 
+# sudo apt-get install -y libmlx4-1 infiniband-diags ibutils ibverbs-utils rdmacm-utils perftest
+sudo apt-get install -y libmlx5-1 infiniband-diags ibutils ibverbs-utils rdmacm-utils perftest
+sudo apt-get install -y tgt
+# sudo apt-get install -y targetcli
+sudo apt-get install -y open-iscsi-utils open-iscsi
+
+
 # RDMA stack modules
 sudo modprobe rdma_cm
 sudo modprobe ib_uverbs
