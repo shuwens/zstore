bb2a270b813139447dda1b77c80be9c3c31983f9
Shuwen Sun <sun.shuw@northeastern.edu>
2024-11-05 16:41:23 +0000

    Checkpoint: 180k

--- src/include/http_server.h
+++ src/include/http_server.h
@@ -108,17 +108,18 @@ auto awaitable_on_request(HttpRequest req,
         //     MakeReadRequest(&zctrl_, dev3, entry.third_lba(),
         //     req).value();
 
-        // auto res = co_await zoneRead(s1);
+        // this yields 180k tho
+        auto res = co_await zoneRead(s1);
 
         s1->Clear();
         zctrl_.mRequestContextPool->ReturnRequestContext(s1);
 
         // co_await (zoneRead(s1) && zoneRead(s2) && zoneRead(s3));
 
-        // without this co_await yields 490k, with co_await yields 70k
-        // co_await async_sleep(co_await boost::asio::this_coro::executor,
-        //                      std::chrono::nanoseconds(1),
-        //                      boost::asio::use_awaitable);
+        // without this co_await yields 550k, with co_await yields 430k
+        co_await async_sleep(co_await boost::asio::this_coro::executor,
+                             std::chrono::microseconds(1),
+                             boost::asio::use_awaitable);
 
         // }
         // zctrl_.queue_depth--;
