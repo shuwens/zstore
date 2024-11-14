50e985887ec620e273e426d004ff95a57b598e18
Shuwen Sun <sun.shuw@northeastern.edu>
2024-11-14 16:55:02 +0000

    fix

--- utils/reformat_device.sh
+++ utils/reformat_device.sh
@@ -4,18 +4,18 @@ echo "Harcoded for one drive, please check the script"
 
 DEV=$1
 #1TB
-nvme delete-ns /dev/$DEV --namespace-id 0xffffffff
-nvme create-ns /dev/$DEV --nsze 0x80000 --ncap 0x80000 --flbas 4 --nmic 1 --csi 0
-nvme attach-ns /dev/$DEV --namespace-id 1 --controllers 0
-nvme create-ns /dev/$DEV --nsze 0x1C400000 --ncap 0x1C400000 --flbas 4 --nmic 1 --csi 2
-nvme attach-ns /dev/$DEV --namespace-id 2 --controllers 0
+# nvme delete-ns /dev/$DEV --namespace-id 0xffffffff
+# nvme create-ns /dev/$DEV --nsze 0x80000 --ncap 0x80000 --flbas 4 --nmic 1 --csi 0
+# nvme attach-ns /dev/$DEV --namespace-id 1 --controllers 0
+# nvme create-ns /dev/$DEV --nsze 0x1C400000 --ncap 0x1C400000 --flbas 4 --nmic 1 --csi 2
+# nvme attach-ns /dev/$DEV --namespace-id 2 --controllers 0
 
 
 #4TB
-# nvme delete-ns /dev/$DEV --namespace-id 0xffffffff
-# nvme create-ns /dev/$DEV --nsze 0x100000 --ncap 0x100000 --flbas 4 --nmic 1 --csi 0
-# nvme attach-ns /dev/$DEV --namespace-id 1 --controllers 0
-# nvme create-ns /dev/$DEV --nsze 0x73400000 --ncap 0x73400000 --flbas 4 --nmic 1 --csi 2
-# nvme attach-ns /dev/$DEV --namespace-id 2 --controllers 0
+nvme delete-ns /dev/$DEV --namespace-id 0xffffffff
+nvme create-ns /dev/$DEV --nsze 0x100000 --ncap 0x100000 --flbas 4 --nmic 1 --csi 0
+nvme attach-ns /dev/$DEV --namespace-id 1 --controllers 0
+nvme create-ns /dev/$DEV --nsze 0x73400000 --ncap 0x73400000 --flbas 4 --nmic 1 --csi 2
+nvme attach-ns /dev/$DEV --namespace-id 2 --controllers 0
 
 
