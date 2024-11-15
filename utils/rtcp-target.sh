38ef60e8f1ab8b3567db941bb9edece7709557e2
Shuwen Sun <sun.shuw@northeastern.edu>
2024-11-15 14:10:20 +0000

    foo

--- utils/rtcp-target.sh
+++ utils/rtcp-target.sh
@@ -44,15 +44,19 @@ sleep 3
 ctrl_nqn="nqn.2024-04.io.zstore:cnode1"
 
 if [ "$HOSTNAME" == "zstore1" ]; then
+	sudo ifconfig enp1s0 12.12.12.1/24 up
 	pci1=05:00.0
 	pci2=0b:00.0
 elif [ "$HOSTNAME" == "zstore2" ]; then
+	sudo ifconfig enp1s0 12.12.12.2/24 up
 	pci1=05:00.0
 	pci2=06:00.0
 elif [ "$HOSTNAME" == "zstore3" ]; then
+	sudo ifconfig enp1s0 12.12.12.3/24 up
 	pci1=04:00.0
 	pci2=06:00.0
 elif [ "$HOSTNAME" == "zstore4" ]; then
+	sudo ifconfig enp1s0 12.12.12.4/24 up
 	pci1=05:00.0
 	pci2=0b:00.0
 fi
