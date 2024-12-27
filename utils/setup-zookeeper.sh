61cf9b34fe6cd427233070c6638cfae7aac3fc2b
Shuwen Sun <sun.shuw@northeastern.edu>
2024-12-27 03:10:59 +0000

    ok

--- utils/setup-zookeeper.sh
+++ utils/setup-zookeeper.sh
@@ -7,9 +7,9 @@ zstore_dir=$(git rev-parse --show-toplevel)
 source $zstore_dir/.env
 cd 
 
-sudo snap install ant --classic
-sudo apt install openjdk-8-jdk-headless maven
-sudo apt-get install libcppunit-dev autoconf automake libtool
+sudo snap install ant --classic -y
+sudo apt install openjdk-8-jdk-headless maven -7
+sudo apt-get install libcppunit-dev autoconf automake libtool -y
 
 mkdir -p tools
 cd tools 
