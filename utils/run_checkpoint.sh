49b4ff5a2933e4f64222120db27c14008747f2a4
Shuwen Sun <sun.shuw@northeastern.edu>
2025-01-10 11:35:11 -0500

    foo

--- utils/run_checkpoint.sh
+++ utils/run_checkpoint.sh
@@ -6,25 +6,25 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-
 for node in zstore1 zstore3 zstore4 zstore5 zstore6
 do
-	echo -e $node "git pull...."
+	ssh ${node} "sudo killall -r 'zstore' || true"
 	ssh ${node} "cd ~/dev/zstore/ && git pull"
 done
-sleep 10
 
+ssh zstore1 "cd ~/tools/apache-zookeeper-3.9.3-bin && ./bin/zkServer.sh start conf/zoo.cfg &"
 
+sleep 1
 
 echo -e "${GREEN}Starting ${RED}run.py${GREEN}...${NC}"
 
 for node in zstore1 zstore3 zstore4 zstore5
 do
 	echo -e $node "start...."
-	ssh ${node} "~/dev/zstore/utils/run_gateway.sh $node"
+	ssh ${node} "~/dev/zstore/utils/run_gateway.sh $node & " &
 done
 sleep 10
 
-ssh zstore6 '~/dev/zstore/utils/run_bench.sh'
+ssh zstore6 '~/dev/zstore/utils/bench_read.sh'
 
 echo "Done"
