a7e71a223fef8930933f00394e631ae68a6f6f88
Shuwen Sun <sun.shuw@northeastern.edu>
2025-01-10 11:15:18 -0500

    git + ipynb = :(

--- utils/run_checkpoint.sh
+++ utils/run_checkpoint.sh
@@ -6,16 +6,25 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+
+for node in zstore1 zstore3 zstore4 zstore5 zstore6
+do
+	echo -e $node "git pull...."
+	ssh ${node} "cd ~/dev/zstore/ && git pull"
+done
+sleep 10
+
+
+
 echo -e "${GREEN}Starting ${RED}run.py${GREEN}...${NC}"
 
 for node in zstore1 zstore3 zstore4 zstore5
-	do
-		echo -e $node "killall...."
-		ssh -f ${node} ~/dev/zstore/utils/run_gateway.sh $node &
-	done
-fi
+do
+	echo -e $node "start...."
+	ssh ${node} "~/dev/zstore/utils/run_gateway.sh $node"
+done
 sleep 10
 
-ssh -f zstore6 ~/dev/zstore/utils/run_bench.sh
+ssh zstore6 '~/dev/zstore/utils/run_bench.sh'
 
 echo "Done"
--- utils/run_gateway.sh
+++ utils/run_gateway.sh
@@ -2,13 +2,13 @@
 set -xeuo pipefail
 
 
-if [ $1== "zstore1" ]; then
+if [ $1 == "zstore1" ]; then
 	cd ~/dev/zstore/build; meson compile; sudo ./zstore 6 1
-elif [ $1== "zstore3" ]; then
+elif [ $1 == "zstore3" ]; then
 	cd ~/dev/zstore/build; meson compile; sudo ./zstore 6 3
-elif [ $1== "zstore4" ]; then
+elif [ $1 == "zstore4" ]; then
 	cd ~/dev/zstore/build; meson compile; sudo ./zstore 6 4
-elif [ $1== "zstore5" ]; then
+elif [ $1 == "zstore5" ]; then
 	cd ~/dev/zstore/build; meson compile; sudo ./zstore 6 5
 fi
 #
