9852f94aa4baa6d89ebad1dc0c544c29054d4363
Shuwen Sun <sun.shuw@northeastern.edu>
2025-02-07 22:15:13 +0000

    wip

--- scripts/autorun_parse.sh
+++ scripts/autorun_parse.sh
@@ -3,15 +3,15 @@
 # that contains newline-separated process names, formatted like like so:
 # <app> <# of gateway> <object size pages> <workload> <# of process>
 # Example:
-#   zstore 1 wrk_read 10
-#   ceph 1 wrk_read 10
-#   ceph 1 wrk_read 10
+#   zstore small wrk_read 10
+#   ceph medium wrk_write 10
+#   minio large s3bench 10
 source $(dirname $0)/utils.sh
 
 # Get the config parameter specified in $1 for app = $autorun_app
 function get_from_config() {
   config_file=$autorun_home/scripts/config/$autorun_app
-  if [ ! -f $config_file ]; then
+  if [ ! -f "$config_file" ]; then
     blue "autorun: App config file $config_file not found. Existing."
     exit
   fi
@@ -34,13 +34,13 @@ export autorun_app=`cat $autorun_home/scripts/autorun_app_file`
 # assert_file_exists $autorun_home/build/$autorun_app
 
 # Variables exported by this script
-# autorun_out_prefix="/tmp/${autorun_app}_out"
-# autorun_err_prefix="/tmp/${autorun_app}_err"
+autorun_out_prefix="/tmp/${autorun_app}_out"
+autorun_err_prefix="/tmp/${autorun_app}_err"
 
 autorun_object_size_pages=`get_from_config "object_size_pages"`
-
 autorun_num_processes=`get_from_config "num_processes"`
 autorun_num_gateways=`get_from_config "num_gateways"`
+autorun_ip_addr=`get_from_config "ip_addr"`
 
 INPUT=`get_from_config "app"`
 autorun_app_name=$(echo $INPUT| cut -d'_' -f 2)
@@ -48,34 +48,5 @@ autorun_app_name=$(echo $INPUT| cut -d'_' -f 2)
 INPUT=`get_from_config "workload"`
 autorun_gen=$(echo $INPUT| cut -d'_' -f 1)
 autorun_workload=$(echo $INPUT| cut -d'_' -f 2)
-
-# autorun_process_file=$(dirname $0)/autorun_process_file
-# if [ ! -f $autorun_process_file ]; then
-#   blue "autorun: Server file not found. Exiting."
-#   exit
-# fi
-
-# Check if the process file has sufficient processs
-# autorun_process_file_num_processes=`cat $autorun_process_file | wc -l`
-# if [ "$autorun_process_file_num_processes" -lt  "$autorun_num_processes" ]; then
-#   blue "autorun: Too few processs in process file. Exiting."
-#   exit
-# fi
-
-# Create autorun process arrays using the first $autorun_num_processes process
-# names in autorun_process_file.
-# process_idx="0"
-# while read dns_name udp_port numa; do
-#   if [[ -z $dns_name || -z $udp_port || -z $numa ]]; then
-#     blue "Invalid autorun file. name = $dns_name, udp = $udp_port, numa = $numa"
-#     blue "Exiting"
-#     exit
-#   fi
-#
-#   autorun_name_list[$process_idx]=$dns_name
-#   autorun_udp_port_list[$process_idx]=$udp_port
-#   autorun_numa_list[$process_idx]=$numa
-#   ((process_idx+=1))
-# done < $autorun_process_file
 
 blue "autorun: app = $autorun_app_name, num gateways = $autorun_num_gateways, generator = $autorun_gen, workload = $autorun_workload, num_processes = $autorun_num_processes"
--- scripts/new_bench.sh
+++ scripts/new_bench.sh
@@ -1,27 +1,19 @@
 #!/usr/bin/env bash
-source $(dirname $0)/utils.sh
-source $(dirname $0)/autorun_parse.sh
 set -xeuo pipefail
 
-if [ ${autorun_app_name} == "zstore" ]; then
-  ip="1"
-elif [ ${autorun_app_name} == "zstore" ]; then
-  ip="2"
-else
-  echo "Invalid app name"
-  exit 1
-fi
+source $(dirname $0)/utils.sh
+source $(dirname $0)/autorun_parse.sh
 
 if [ ${autorun_gen} == "wrk" ]; then
   echo "Using wrk"
   if [ ${autorun_workload} == "read" ]; then
     sudo taskset -c 1-${autorun_num_processes} ~/tools/wrk/wrk -t${autorun_num_processes} -c${autorun_num_connections} -d5s \
       -H 'Connection: keep-alive' --latency --timeout 60 \
-      -s random-reads.lua http://12.12.12.${ip}:2000 -- 100000 false
+      -s random-reads.lua ${autorun_ip_addr} -- 100000 false
   elif [ ${autorun_workload} == "write" ]; then
     sudo taskset -c 1-${autorun_num_processes} ~/tools/wrk/wrk -t${autorun_num_processes} -c${autorun_num_connections} -d1s \
       -H 'Connection: keep-alive' --latency --timeout 60 \
-      -s random-reads.lua http://12.12.12.${ip}:2000 -- 100000 false
+      -s random-reads.lua ${autorun_ip_addr} -- 100000 false
   fi
 elif [ $autorun_gen == "s3bench" ]; then
   echo "Using s3bench"
@@ -29,5 +21,3 @@ else
   echo "Invalid autorun_gen"
   exit 1
 fi
-
-
